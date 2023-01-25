ea0772a9a9a6417c5c20faa3a34ca514035b9f53
AEMRE <k.aliemre@gmail.com>
2023-01-26 00:18:37 +0300

    Add files via upload

---
+++ Models/UserSessionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AkilliBot.Areas.Admin.Models
+{
+    public class UserSessionModel
+    {
+        public string Username { get; set; }
+    }
+}
